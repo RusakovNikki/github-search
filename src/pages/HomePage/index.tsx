d0a2837559105bd056e9a26fc834f012bb355f26
Русаков Никита Станиславович <n.rusakov@unicon.ru>
2024-04-11 23:11:23 +0300

    feat[HomePage]: using throttle

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,17 +1,17 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../../components/feature/Card';
 import TextField from '../../components/feature/TextField';
 import Button from '../../components/core/Button';
 import { observer } from 'mobx-react-lite';
 import repositoryStore from '../../store/repositoryStore';
-import { useDebounce } from '../../hooks/useDebounce';
 import Snackbar from '../../components/core/Snackbar';
+import { useThrottle } from '../../hooks/useThrottle';
 
 const HomePage = () => {
   const { getRepositoriesByName, isError, isLoading, repositories } =
     repositoryStore;
   const [searchText, setSearchText] = useState<string>('');
-  const debouncedSearch = useDebounce(searchText);
+  const debouncedSearch = useThrottle(searchText, 1000);
   const [repositoriesPage, setRepositoriesPage] = useState<number>(1);
   const [openSnackbar, setOpenSnackbar] = useState<boolean>(false);
 
@@ -27,7 +27,6 @@ const HomePage = () => {
       perPage: 10,
     });
   }, [debouncedSearch, getRepositoriesByName, repositoriesPage]);
-  console.log(repositories?.items.length);
 
   return (
     <div style={{ display: 'flex', gap: '1rem', flexDirection: 'column' }}>
@@ -48,8 +47,8 @@ const HomePage = () => {
         />
         <Button>Копировать</Button>
       </div>
-      {repositories?.items.length ? (
-        repositories.items.map((repositoryItem) => (
+      {repositories ? (
+        repositories?.items?.map((repositoryItem) => (
           <Card
             title1="Stars count"
             value1={repositoryItem.stargazers_count.toString()}
