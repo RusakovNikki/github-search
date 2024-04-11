b5d7524831cef8e5815ba125d287137f9685bef2
Русаков Никита Станиславович <n.rusakov@unicon.ru>
2024-04-11 21:59:52 +0300

    feat[HomePage]: showing data by request

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,10 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../../components/feature/Card';
 import TextField from '../../components/feature/TextField';
 import Button from '../../components/core/Button';
+import { observer } from 'mobx-react-lite';
+import repositoryStore from '../../store/repositoryStore';
+import { useDebounce } from '../../hooks/useDebounce';
 
 const HomePage = () => {
+  const { getRepositoriesByName, isError, isLoading, repositories } =
+    repositoryStore;
   const [searchText, setSearchText] = useState<string>('');
+  const debouncedSearch = useDebounce(searchText);
+  const [repositoriesPage, setRepositoriesPage] = useState<number>(1);
+
+  useEffect(() => {
+    getRepositoriesByName(debouncedSearch, {
+      page: repositoriesPage,
+      perPage: 10,
+    });
+  }, [debouncedSearch, getRepositoriesByName, repositoriesPage]);
 
   return (
     <div style={{ display: 'flex', gap: '1rem', flexDirection: 'column' }}>
@@ -17,19 +31,27 @@ const HomePage = () => {
           justifyContent: 'space-between',
         }}
       >
-        <TextField label="Поиск" placeholder="github-search" />
+        <TextField
+          label="Поиск"
+          placeholder="github-search"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
         <Button>Копировать</Button>
       </div>
-      <Card
-        title1="ссылку на репозиторий, текст ссылки - полное название репозитория"
-        title2="число старов"
-        title3="число форков"
-        mainTitle="redbell"
-        imageLink="https://avatars.githubusercontent.com/u/79338989?v=4"
-        mainDescription="Big Techs grip on social media is a growing problem"
-      />
+      {repositories?.items.map((repositoryItem) => (
+        <Card
+          title1="Stars count"
+          value1={repositoryItem.stargazers_count.toString()}
+          title2="Forks count"
+          value2={repositoryItem.forks_count.toString()}
+          mainTitle={repositoryItem.full_name}
+          imageLink={repositoryItem.owner.avatar_url}
+          mainDescription={repositoryItem.html_url}
+        />
+      )) ?? undefined}
     </div>
   );
 };
 
-export default HomePage;
+export default observer(HomePage);
